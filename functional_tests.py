cd131094cfe329fc3e7fc6cb466afb6570e0c9d6
luizcfernandes <lzcsfs@gmail.com>
2025-12-05 19:20:06 -0300

    Primeiro FT especificado nos comentarios, utilizando agora o unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+import unittest
+
 import time
 
 # --- Define the correct path to the Chrome executable ---
@@ -12,26 +14,38 @@ import time
 # Example for Linux:
 # CHROME_BINARY_LOCATION = "/usr/bin/google-chrome"
 
-# Set your actual, correct path here:
-CHROME_BINARY_LOCATION = "/usr/bin/google-chrome"
+# Set your actual, correct path here
 
-# --- Setup Options ---
-options = Options()
-options.headless = True # Run in background
-# Assign the *verified* path to the binary_location option
-options.binary_location = CHROME_BINARY_LOCATION
 
-# --- Initialize Driver ---
-try:
-    driver = webdriver.Chrome(options=options)
-except Exception as e:
-    print(f"An error occurred: {e}")
-    print("\nPlease verify that the path in 'CHROME_BINARY_LOCATION' is exactly correct.")
+class NewVisitorTest(unittest.TestCase):
 
-driver.get("http://localhost:8000")
+    def setUp(self):
+        CHROME_BINARY_LOCATION = "/usr/bin/google-chrome"
+
+        # --- Setup Options ---
+        options = Options()
+        options.headless = True # Run in background
+        # # Assign the *verified* path to the binary_location option
+        options.binary_location = CHROME_BINARY_LOCATION
+        # # --- Initialize Driver ---
+        try:
+            self.browser = webdriver.Chrome(options=options)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            print("\nPlease verify that the path in 'CHROME_BINARY_LOCATION' is exactly correct.")
+
+    def tearDown(self):
+        return self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
 
 time.sleep(3)
-assert 'install' in driver.title
 
-if __main__ == '__main__':
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
