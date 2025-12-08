cac576e6788daccaf74028d20bdc9a30e0ddd55a
luizcfernandes <lzcsfs@gmail.com>
2025-12-08 13:25:55 -0300

    Make functional_tests an app and use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import unittest
+
+from django.test import LiveServerTestCase
 
 import time
 
@@ -18,7 +19,7 @@ import time
 # Set your actual, correct path here
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         CHROME_BINARY_LOCATION = "/usr/bin/google-chrome"
@@ -44,7 +45,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, 'h1')
@@ -75,6 +76,6 @@ class NewVisitorTest(unittest.TestCase):
 
         self.fail('Finish the test')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+# if __name__ == '__main__':
+#     unittest.main(warnings='ignore')
 
