22ab648dc13d7679bf9c4849f060ad2ae001fa19
luizcfernandes <lzcsfs@gmail.com>
2025-12-09 23:12:56 -0300

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -112,13 +112,13 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis acessa a página inicial. Não há nenhum sinal da lista de Edith
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body').text
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
         # Francis inicia uma nova lista inserindo um item novo. Ele
         # é menos interessante que Edith...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
@@ -129,7 +129,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Novamente, não há nenhum sinal da lista de Edith
-        page_text = self.browser.find_element_by_tag_name('body').text
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,6 @@ from listas import views
 from django.urls import re_path
 
 urlpatterns = [
-    re_path(r'^$', views.home_page, name='home')
+    re_path(r'^$', views.home_page, name='home'),
+    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
