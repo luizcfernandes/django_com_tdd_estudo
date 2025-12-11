0292c4c2c36f3ebbfa0aaaf86e36b11a223eea1d
luizcfernandes <lzcsfs@gmail.com>
2025-12-11 12:18:11 -0300

    first steps of FT formlayour  + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -134,7 +134,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfeitos, ambos voltam a dormir
-        self.fail('Finish the test')
+        # self.fail('Finish the test')
+
+    def test_layout_and_styling(self):
+        # Edith acessa a página inicial
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela percebe que a caixa de entrada está elegantemente centralizada
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+
+        # Ela inicia uma nova lista e vê que a entrada está elegantemente
+        # centralizada aí também
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+
+        time.sleep(3)
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
 
 # if __name__ == '__main__':
 #     unittest.main(warnings='ignore')
